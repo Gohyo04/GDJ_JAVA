2d0359270901f81d9941607a61fa170f0d7113d6
Gohyo04 <gohyo7@gmail.com>
2023-12-05 14:34:35 +0900

    2

--- src/com/gohyo/study1/Study4_For.java
+++ src/com/gohyo/study1/Study4_For.java
@@ -2,6 +2,10 @@ package com.gohyo.study1;
 
 public class Study4_For {
 	public static void main(String[] args) {
-		System.out.println("Master");
+		// 서든 FPS
+		// 총알 : 30발 * 3
+		
+		// 1. 단발모드
+		// 2. 점사모드
 	}
 }
