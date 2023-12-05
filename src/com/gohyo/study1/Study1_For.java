18b97928e18f3de634e9f63f7f92bf7a78185c90
Gohyo04 <gohyo7@gmail.com>
2023-12-05 11:49:56 +0900

    add2

--- src/com/gohyo/study1/Study1_For.java
+++ src/com/gohyo/study1/Study1_For.java
@@ -10,12 +10,15 @@ public class Study1_For {
 		System.out.println("정수 하나를 입력하세요");
 		
 		int num = sc.nextInt();
-		
+		int cnt = 0;
 		// 입력받은 정수의 약수를 출력하세요
+		// 약수의 갯수를 출력하세요
 		for(int i=1;i<=num;i++) {
 			if(num % i == 0) {
 				System.out.println(i);
+				cnt++;
 			}
 		}
+		System.out.println("약수의 갯수: "+cnt);
 	}
 }
