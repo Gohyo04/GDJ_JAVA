76026a80207da9490e451f84ab6f3f4eeb93a26a
Gohyo04 <gohyo7@gmail.com>
2023-12-12 17:47:10 +0900

    1212_h

--- src/com/gohyo/study3/school/SchoolMain.java
+++ src/com/gohyo/study3/school/SchoolMain.java
@@ -8,5 +8,25 @@ public class SchoolMain {
 		SchoolController controller = new SchoolController();
 		
 		controller.start();
+		
+//		stack heap
+//		stack 방식으로 정보가 전달되게(LIFO)
+//		que 방식으로 전달 (FIFO)
+//
+//		lifecycle 개념
+//
+//		startup.bat
+//
+//		ip주소(domain):port번호
+//
+//		파라미터 : client -> server
+//
+//		this()
+//		super()
+//
+//		js
+//		classList
+//		target
+//		parentNode
 	}
 }
