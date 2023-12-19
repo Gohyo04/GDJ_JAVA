3ea1002ad531364732a03ccaa57980f96924c844
Go <gohyo4@gmail.com>
2023-12-20 00:33:14 +0900

    1219_h2

--- src/com/gohyo/study6/ex2/StudentDAO.java
+++ src/com/gohyo/study6/ex2/StudentDAO.java
@@ -1,10 +1,47 @@
 package com.gohyo.study6.ex2;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class StudentDAO {
-	//1. 파일의 내용을 한줄씩 읽어오기
 	
-	//2. 한줄의 내용을 파싱
-	//3. 파싱한 토큰을 DTO에 생성하고 멤버변수에 대입
-	//4. DTO를 List에 추가
-	//5. List에 return
+	public ArrayList<StudentDTO> getStudent() throws Exception{
+		// 파일 가져온다
+		File file = new File("C:\\study\\student.txt");
+		// 파일을 읽는다
+		FileReader fr = new FileReader(file);
+		// 파일 내용을 한줄씩 가져온다 
+		BufferedReader br = new BufferedReader(fr);
+		// 첫줄 설명 빼기
+		br.readLine();
+		ArrayList<StudentDTO> sdtos = new ArrayList<>();
+		
+		//1. 파일의 내용을 한줄씩 읽어오기
+		while(true) {
+			String str = br.readLine();
+			if(str == null) {
+				break;
+			}
+			//2. 한줄의 내용을 파싱 ( str -> br.readLne()을  - 을 기준으로 문자열 쪼개기 )
+			StringTokenizer st = new StringTokenizer(str,"-");
+			//3. 파싱한 토큰을 DTO에 생성하고 멤버변수에 대입
+			StudentDTO sdto = new StudentDTO();
+			sdto.setName(st.nextToken());
+			sdto.setNum(Integer.parseInt(st.nextToken()));
+			sdto.setKor(Integer.parseInt(st.nextToken()));
+			sdto.setEng(Integer.parseInt(st.nextToken()));
+			sdto.setMath(Integer.parseInt(st.nextToken()));
+			sdto.setSum(Integer.parseInt(st.nextToken()));
+			sdto.setAvg(Double.parseDouble(st.nextToken()));
+			//4. DTO를 List에 추가
+			//set 한 sdto 객체를 배열에 대입
+			sdtos.add(sdto);
+		}
+		//5. List에 return
+		return sdtos;
+	}
 }
--- src/com/gohyo/study6/ex2/StudentMain.java
+++ src/com/gohyo/study6/ex2/StudentMain.java
@@ -1,10 +1,18 @@
 package com.gohyo.study6.ex2;
 
+import java.util.ArrayList;
+
 public class StudentMain {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception{
+		StudentDAO dao = new StudentDAO();
+		
+		dao.getStudent();
+		ArrayList<StudentDTO> list = dao.getStudent();
+		for(StudentDTO dto : list) {
+			System.out.println(dto.getNum()+". "+dto.getName());
+			System.out.println("국어:"+dto.getKor()+" 영어:"+dto.getEng()+" 수학:"+dto.getMath());
+			System.out.println("합계:"+dto.getSum()+" 평균:"+dto.getAvg());
+			System.out.println("==============================");
+		}
 	}
-
 }
